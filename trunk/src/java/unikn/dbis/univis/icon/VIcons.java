2d392ca3425a6516b29a9762d21616c7a3584313
raedler <raedler@a6051bc3-d710-0410-ac7c-d0d72cf6e3c9>
2006-04-18 20:55:51 +0000

    renamed to VIcons

---
+++ trunk/src/java/unikn/dbis/univis/icon/VIcons.java
@@ -0,0 +1,27 @@
+package unikn.dbis.univis.icon;
+
+import javax.swing.*;
+
+/**
+ * TODO: document me!!!
+ * <p/>
+ * <code>VIcons</code>.
+ * <p/>
+ * User: raedler, weiler
+ * Date: 10.04.2006
+ * Time: 17:28:18
+ *
+ * @author Roman R&auml;dle
+ * @author Andreas Weiler
+ * @version $Id$
+ * @since UniVis Explorer 0.1
+ */
+public class VIcons {
+    public static final Icon REFRESH = new ImageIcon(VIcons.class.getResource("../images/refresh.png"));
+    public static final Icon VIEW = new ImageIcon(VIcons.class.getResource("../images/view.png"));
+    public static final Icon UNDO = new ImageIcon(VIcons.class.getResource("../images/undo.png"));
+    public static final Icon REDO = new ImageIcon(VIcons.class.getResource("../images/redo.png"));
+    public static final Icon FUNNEL = new ImageIcon(VIcons.class.getResource("../images/funnel.png"));
+    public static final Icon BARCHART = new ImageIcon(VIcons.class.getResource("../images/barChart.png"));
+    public static final Icon PIECHART = new ImageIcon(VIcons.class.getResource("../images/pieChart.png"));
+}
