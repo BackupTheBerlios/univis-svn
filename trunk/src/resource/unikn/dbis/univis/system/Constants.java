bbeafdb0037517e256d1fa080166b1b9970fcd0a
weilera <weilera@a6051bc3-d710-0410-ac7c-d0d72cf6e3c9>
2006-05-19 11:45:48 +0000

    new

---
+++ trunk/src/resource/unikn/dbis/univis/system/Constants.java
@@ -0,0 +1,21 @@
+package unikn.dbis.univis.system;
+
+/**
+ * TODO: document me!!!
+ * <p/>
+ * Constants.
+ * <p/>
+ * User: raedler, weiler
+ * Date: 19.05.2006
+ * Time: 13:36:33
+ *
+ * @author Roman R&auml;dle
+ * @author Andreas Weiler
+ * @version $Id$
+ * @since UniVis Explorer 0.1
+ */
+public interface Constants {
+
+    public final static String German = "german";
+    public final static String English = "english";
+}
