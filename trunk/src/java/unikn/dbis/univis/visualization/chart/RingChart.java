6bdf3b7dd00c855b2aa68975b97dff4bba2c20c6
weilera <weilera@a6051bc3-d710-0410-ac7c-d0d72cf6e3c9>
2006-05-21 20:33:17 +0000

    new Chart

---
+++ trunk/src/java/unikn/dbis/univis/visualization/chart/RingChart.java
@@ -0,0 +1,79 @@
+package unikn.dbis.univis.visualization.chart;
+
+import org.jfree.data.general.PieDataset;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.plot.RingPlot;
+import org.jfree.util.Rotation;
+import unikn.dbis.univis.visualization.Renderable;
+
+/**
+ * TODO: document me!!!
+ * <p/>
+ * RingChart.
+ * <p/>
+ * User: raedler, weiler
+ * Date: 21.05.2006
+ * Time: 22:31:06
+ *
+ * @author Roman R&auml;dle
+ * @author Andreas Weiler
+ * @version $Id$
+ * @since UniVis Explorer 0.1
+ */
+public class RingChart extends AbstractChart<PieDataset> implements Renderable {
+
+    // Total amount of the chart.
+    private Integer total;
+
+    /**
+     * A pie chart with the chart name as name of the chart and
+     * the dataset that contains the data of the pie chart.
+     *
+     * @param chartName Headline of the chart.
+     * @param dataset   Dataset of the Chart.
+     */
+    public RingChart(String chartName, PieDataset dataset) {
+        super(chartName, dataset);
+    }
+
+    /**
+     * Returns the JFreeChart.
+     *
+     * @return JFreeChart.
+     */
+    protected JFreeChart createChart() {
+        return ChartFactory.createRingChart(getChartName(), getDataset(), true, false, false);
+    }
+
+    /**
+     * Returns the total amount of the chart.
+     *
+     * @return Total amount of the chart.
+     */
+    protected int createTotal() {
+
+        if (total != null) {
+            return total;
+        }
+
+        total = 0;
+        for (int i = getDataset().getItemCount() - 1; i >= 0; i--) {
+            total += getDataset().getValue(i).intValue();
+        }
+        return total;
+    }
+
+    /**
+     * Makes the plot.
+     */
+    protected void plot() {
+        RingPlot plot = (RingPlot) getChart().getPlot();
+        plot.setStartAngle(290);
+        plot.setDirection(Rotation.CLOCKWISE);
+        plot.setForegroundAlpha(0.5f);
+        plot.setNoDataMessage("No data available");
+        plot.setLabelGenerator(null);
+        plot.setLegendLabelGenerator(new LabelGenerator(createTotal()));
+    }
+}
