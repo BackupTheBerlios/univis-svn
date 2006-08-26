79055d01c031f1424ccb641ee1eaf0b24ca45dc2
weilera <weilera@a6051bc3-d710-0410-ac7c-d0d72cf6e3c9>
2006-08-26 18:07:29 +0000

    VMeasure

---
+++ trunk/src/java/unikn/dbis/univis/meta/VMeasure.java
@@ -0,0 +1,17 @@
+package unikn.dbis.univis.meta;
+
+/**
+ * The <code>VDataReference</code> contains information about
+ * the data of the object that references this object.
+ * <p/>
+ * User: raedler, weiler
+ * Date: 26.08.2006
+ * Time: 15:55:56
+ *
+ * @author Roman R&auml;dle
+ * @author Andreas Weiler
+ * @version $Id$
+ * @since UniVis Explorer 0.2
+ */
+public interface VMeasure extends VDataReference {
+}
