aabaf5d9ba4bfe439b7c4b812ea4138e56541449
weilera <weilera@a6051bc3-d710-0410-ac7c-d0d72cf6e3c9>
2006-08-26 18:07:13 +0000

    VMeasure

---
+++ trunk/src/java/unikn/dbis/univis/meta/impl/VMeasureImpl.java
@@ -0,0 +1,21 @@
+package unikn.dbis.univis.meta.impl;
+
+import unikn.dbis.univis.meta.VMeasure;
+import unikn.dbis.univis.meta.VDataReference;
+
+/**
+ * TODO: document me!!!
+ * <p/>
+ * VMeasureImpl.
+ * <p/>
+ * User: raedler, weiler
+ * Date: 26.08.2006
+ * Time: 15:56:51
+ *
+ * @author Roman R&auml;dle
+ * @author Andreas Weiler
+ * @version $Id$
+ * @since UniVis Explorer 0.2
+ */
+public class VMeasureImpl extends VDataReferenceImpl implements VMeasure, VDataReference {
+}
