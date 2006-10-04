0aec6da5a3329cc61499336d78441ffa3c99acaa
raedler <raedler@a6051bc3-d710-0410-ac7c-d0d72cf6e3c9>
2006-10-04 12:51:03 +0000

    * fixed missing VImageDummy.java

---
+++ trunk/src/java/unikn/dbis/univis/images/VImageDummy.java
@@ -0,0 +1,18 @@
+package unikn.dbis.univis.images;
+
+/**
+ * TODO: document me!!!
+ * <p/>
+ * <code>VImageDummy</code>.
+ * <p/>
+ * User: raedler, weiler
+ * Date: 01.06.2006
+ * Time: 00:14:06
+ *
+ * @author Roman R&auml;dle
+ * @author Andreas Weiler
+ * @version $Id$
+ * @since UniVis Explorer 0.1
+ */
+public class VImageDummy {
+}
