44e1cef2f531d1f1b6661a9992103c60d36afbef
raedler <raedler@a6051bc3-d710-0410-ac7c-d0d72cf6e3c9>
2006-10-08 22:52:13 +0000

    * Id keyword test

---
+++ trunk/src/java/unikn/dbis/univis/explorer/VLicense.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2005-2006 UniVis Explorer development team.
+ *
+ * This file is part of UniVis Explorer
+ * (http://phobos22.inf.uni-konstanz.de/univis).
+ *
+ * UniVis Explorer is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * Please see COPYING for the complete licence.
+ */
+package unikn.dbis.univis.explorer;
+
+/**
+ * TODO: document me!!!
+ * <p/>
+ * <code>VLicense</code>.
+ * <p/>
+ * User: raedler
+ * Date: 09.10.2006
+ * Time: 00:51:36
+ *
+ * @author Roman R&auml;dle
+ * @version $Id$
+ * @since UniVis Explorer 0.3
+ */
+public class VLicense {
+}
